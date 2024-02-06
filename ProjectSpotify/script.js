9c0a77d8a955b105c3bf6ac036d49968d5351f21
peije21 <fabiotwist1@gmail.com>
2024-02-05 23:37:23 -0300

    add initial of a responsive mob layout

--- ProjectSpotify/script.js
+++ ProjectSpotify/script.js
@@ -2,11 +2,38 @@ const srcInput = document.getElementById("srcInput")
 const srcArtist = document.getElementById("srcArtist")
 const playlistItems = document.getElementById("playlists-items")
 
-srcInput.addEventListener("input",() => {
-    const input = srcInput.value.toLowerCase()
-    if(input === " "){
-        playlistItems.classList.add = "hidden"
-        srcArtist.classList.remove = "hidden"
+function reqDeApi(input){
+    const url = `http://localhost:3000/artists?name_like=${input}`
+    try{
+        fetch(url).then((res) => res.json()).then(data => mostrarResultados(data))
+    } catch(e){
+        console.log(e)
     }
-    return;
+}      
+
+function mostrarResultados(data){
+    srcArtist.innerHTML = ""
+    data.map(element => {
+        srcArtist.innerHTML +=`
+            <a href="" class="text-btn search__artist--item">
+                <picture>
+                    <img src="${element.urlImg}" alt="artist-logo" id="search__artist--logo">
+                </picture>
+                <span id="search__artist--name">${element.name}</span>
+                <span> Artista </span>
+            </a>
+        `
+    });
+}
+
+srcInput.addEventListener("input",function () {
+    const input = srcInput.value.toLowerCase()
+    if(input != ""){
+        playlistItems.classList.add("hidden")
+        srcArtist.classList.remove("hidden")
+    } else{
+        playlistItems.classList.remove("hidden")
+        srcArtist.classList.add("hidden")
+    }
+    reqDeApi(input)
 })
